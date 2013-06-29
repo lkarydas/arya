8e196f167f13b107baea940b9aa4db649f8bf8e1
Laz <lkarydas@gmail.com>
2013-06-28 21:03:40 -0700

    Code cleanup

--- src/com/laz/arya/MyRenderer.java
+++ src/com/laz/arya/MyRenderer.java
@@ -12,82 +12,73 @@ public class MyRenderer implements Renderer {
 
 	Triangle mTriangle;
 	Sphere mSphere;
-    
-    private final float[] mMVPMatrix = new float[16];
-    private final float[] mProjMatrix = new float[16];
-    private final float[] mVMatrix = new float[16];
-    private final float[] mRotationMatrix = new float[16];
-    // Declare as volatile because we are updating it from another thread
-    public volatile float mAngle;
-    
-    private final float[] cameraPosition = new float[3];
-    
-    Camera3D camera;
-	
-    public void moveCamera(float step){
-    	cameraPosition[2] += step*10;
-    }
+
+	private final float[] mProjMatrix = new float[16];
+	// Declare as volatile because we are updating it from another thread
+	public volatile float mAngle;
+
+	private final float[] cameraPosition = new float[3];
+
+	Camera3D camera;
+
+	public void moveCamera(float step){
+		cameraPosition[2] += step*10;
+	}
 
 	@Override
 	public void onDrawFrame(GL10 gl) {
 
-
-		
-	      // Redraw background color
-        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
-        
-
+		// Redraw background color
+		GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
 		camera.update();
-        
-        mSphere.draw(camera.combined);
+		
+		camera.printViewMatrix();
+		
+		mSphere.draw(camera.combined);
 
 	}
 
 	@Override
 	public void onSurfaceChanged(GL10 gl, int width, int height) {
 		GLES20.glViewport(0, 0, width, height);
-		
-		float ratio = (float) width / height;
-		
 
+		float ratio = (float) width / height;
 		// This projection matrix is applied to object coordinates
 		// in the onDrawFrame() method
 		Matrix.frustumM(mProjMatrix, 0, -ratio, ratio, -1, 1, 3, 7);
-
 	}
 
 	@Override
 	public void onSurfaceCreated(GL10 gl, EGLConfig config) {
-		
+
 		camera = new Camera3D();
 
-		
-	    // initialize a triangle
-	    mTriangle = new Triangle();
-	    
-	    mSphere = new Sphere(1f, (short) 10, (short) 10);
-		
-	     // Set the background frame color
-        GLES20.glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
-        
-        // Initialize camera position
-        cameraPosition[0] = 0;
-        cameraPosition[1] = 0;
-        cameraPosition[2] = -4;
+		// initialize a triangle
+		mTriangle = new Triangle();
+
+		mSphere = new Sphere(1f, (short) 10, (short) 10);
+
+		// Set the background frame color
+		GLES20.glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
+
+		// Initialize camera position
+		cameraPosition[0] = 0;
+		cameraPosition[1] = 0;
+		cameraPosition[2] = -4;
 
 	}
-	
+
 	public static int loadShader(int type, String shaderCode){
 
-	    // create a vertex shader type (GLES20.GL_VERTEX_SHADER)
-	    // or a fragment shader type (GLES20.GL_FRAGMENT_SHADER)
-	    int shader = GLES20.glCreateShader(type);
+		// create a vertex shader type (GLES20.GL_VERTEX_SHADER)
+		// or a fragment shader type (GLES20.GL_FRAGMENT_SHADER)
+		int shader = GLES20.glCreateShader(type);
 
-	    // add the source code to the shader and compile it
-	    GLES20.glShaderSource(shader, shaderCode);
-	    GLES20.glCompileShader(shader);
+		// add the source code to the shader and compile it
+		GLES20.glShaderSource(shader, shaderCode);
+		GLES20.glCompileShader(shader);
 
-	    return shader;
+		return shader;
 	}
 
 }
